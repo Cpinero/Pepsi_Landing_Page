2df9d17b9b28bb8e86f36cb7484e63ef6a8f4b12
Cpinero <37397139+Cpinero@users.noreply.github.com>
2021-06-06 03:20:49 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function imgSlider(img) {
+    document.querySelector('.pepsi').src = img;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
